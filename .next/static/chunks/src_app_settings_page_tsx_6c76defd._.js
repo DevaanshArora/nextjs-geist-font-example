fb8eb09ef2db393e1aaa17164fe8ecf92f006657
DevaanshArora <81051355+DevaanshArora@users.noreply.github.com>
2025-07-16 18:02:25 +0530

    Add .next/static/chunks/src_app_settings_page_tsx_6c76defd._.js 1752669144580

---
+++ .next/static/chunks/src_app_settings_page_tsx_6c76defd._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_3e851847._.js",
+  "static/chunks/node_modules_81252552._.js"
+],
+    source: "dynamic"
+});
